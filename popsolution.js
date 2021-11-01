f846499926a7a2c1647b30cc478d9b9f370b9428
byronwmccoy <93497716+byronwmccoy@users.noreply.github.com>
2021-10-31 22:22:24 -0400

    qq update

--- popsolution.js
+++ popsolution.js
@@ -1,5 +1,4 @@
-//change 5 to the total number of questions
-var total=5
+var total=10
 var question=new Array()
 for (i=1;i<=total+1;i++){
 temp="choice"+i+"=new Array()"
@@ -18,39 +17,78 @@ compliments[6]="Good work!"
 
 
 
-question[1]="What is the difference between a jungle and a rain forest?"
-choice1[1]="No difference. Simply two different ways in referring to the same thing."
-choice1[2]="A jungle in general receives less rain than a rain forest."
-choice1[3]="A jungle refers to the thickest area of a rain forest"
-choice1[4]="A jungle and a rain forest each contain their own group of distinct plants and animals."
+question[1]="Who is the current President of the University of Florida?"
+choice1[1]="Dr. Fawkes"
+choice1[2]="Dr. Fox"
+choice1[3]="Dr. Fuchs"
+choice1[4]="Dr. Forks"
 
-question[2]="What is the world's most common religion?"
-choice2[1]="Christianity"
-choice2[2]="Buddhism"
-choice2[3]="Hinduism"
-choice2[4]="Muslim"
+question[2]="Who is the current Vice President for Human Resources at the University of Florida?"
+choice2[1]="Jodi Gentry"
+choice2[2]="Jordan Garrety"
+choice2[3]="Jon Gordon"
+choice2[4]="Jodie Greene"
 
-question[3]="Which city ranks as the world's most populous city?"
-choice3[1]="New York (US)"
-choice3[2]="Mexico City (Mexico)"
-choice3[3]="Tokyo (Japan)"
-choice3[4]="Shanghai (China)"
+question[3]="Who is the current Vice President for Research at the University of Florida?"
+choice3[1]="Dr. Nelson"
+choice3[2]="Dr. Norton"
+choice3[3]="Dr. Mitchell"
+choice3[4]="Dr. Cooke"
 
-question[4]="According to statistics, what kind of sites on the net are the most popular?"
-choice4[1]="Adult content sites"
-choice4[2]="Portal sites"
-choice4[3]="Chat sites"
-choice4[4]="News sites"
+question[4]="Who is the current Senior Vice President for Health Affairs at the University of Florida?"
+choice4[1]="Dr. Norton"
+choice4[2]="Dr. Nelson"
+choice4[3]="Dr. Koch"
+choice4[4]="Dr. Sugrue"
 
-question[5]="As of June 1998, how much is Microsoft Chairman Bill Gates's net worth?"
-choice5[1]="10 million US"
-choice5[2]="10 billion US"
-choice5[3]="35 billion US"
-choice5[4]="50 billion US"
+question[5]="Which other organization reports to the SVP for Health Affairs?"
+choice5[1]="North Florida Regional Medical Center"
+choice5[2]="Meridian Behavioral Healthcare"
+choice5[3]="Select Medical Hospitals"
+choice5[4]="UF Health"
+
+
+question[6]="Which of these UF organizations does not fall under the umbrella of the SVP for Health Affairs?"
+choice6[1]="UF Health Cancer Center"
+choice6[2]="College of Medicine"
+choice6[3]="Department of Health Outcomes & Biomedical Informatics"
+choice6[4]="Office of Research"
+
+
+question[7]="Who is the current Director of the UF Health Cancer Center?"
+choice7[1]="Dr. Licht"
+choice7[2]="Dr. Nelson"
+choice7[3]="Dr. Jobin"
+choice7[4]="Dr. Wingard"
+
+
+question[8]="Which of these is not an Office within the UFHCC?"
+choice8[1]="Office of the Director"
+choice8[2]="Clinical Research Office"
+choice8[3]="Office of Clinical Affairs"
+choice8[4]="Office of Research Administration"
+
+
+question[9]="Administration is led by which member of the UFHCC Executive Leadership team?"
+choice9[1]="Dr. George"
+choice9[2]="Dr. Houlihan"
+choice9[3]="Dr. Siemann"
+choice9[4]="Dr. Shenkman"
+
+
+question[10]="The UFHCC-HR team is led by which of the following Cancer Center managers?"
+choice10[1]="Joshua Shaffer"
+choice10[2]="Byron McCoy"
+choice10[3]="Holly O'Ferrell"
+choice10[4]="Ashley Williams"
 
 solution[1]="c"
 solution[2]="a"
 solution[3]="b"
 solution[4]="b"
 solution[5]="d"
-
+solution[6]="d"
+solution[7]="a"
+solution[8]="c"
+solution[9]="b"
+solution[10]="b"
